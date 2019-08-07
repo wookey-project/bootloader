3d2dc052923a05e9c7d0b694b6bdecd885c9a4b4
Philippe Thierry <phil@reseau-libre.net>
2019-08-07 14:35:23 +0200

    [bugfix] typos

--- src/main.h
+++ src/main.h
@@ -34,7 +34,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-eoid exti_button_handler(uint8_t irq __attribute__((unused)),
+void exti_button_handler(uint8_t irq __attribute__((unused)),
                          uint32_t sr __attribute__((unused)),
                          uint32_t dr __attribute__((unused)));
 
